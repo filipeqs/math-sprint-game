e3dee6d2a0ddb3ac893d0c5abb1d108b732a12b9
Filipe Silva <filipeqs@outlook.com>
2021-07-13 19:36:53 -0400

    equations to dom

--- script.js
+++ script.js
@@ -34,6 +34,12 @@ const wrongFormat = [];
 
 // Scroll
 
+// Displays Game Page
+const showGamePage = () => {
+    gamePage.hidden = false;
+    countdownPage.hidden = true;
+};
+
 // Get Random Number up to a Max number
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
@@ -67,29 +73,45 @@ function createEquations() {
     }
 
     shuffle(equationsArray);
-    console.log(equationsArray);
 }
 
+// Add Equations to DOM
+const equationsToDOM = () => {
+    equationsArray.forEach((equation) => {
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Equation Text
+        const equationText = document.createElement('h1');
+        equationText.textContent = equation.value;
+        // Append
+        item.appendChild(equationText);
+        itemContainer.appendChild(item);
+    });
+};
+
 // Dynamically adding correct/incorrect equations
-// function populateGamePage() {
-//   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = '';
-//   // Spacer
-//   const topSpacer = document.createElement('div');
-//   topSpacer.classList.add('height-240');
-//   // Selected Item
-//   const selectedItem = document.createElement('div');
-//   selectedItem.classList.add('selected-item');
-//   // Append
-//   itemContainer.append(topSpacer, selectedItem);
+function populateGamePage() {
+    // Reset DOM, Set Blank Space Above
+    itemContainer.textContent = '';
+    // Spacer
+    const topSpacer = document.createElement('div');
+    topSpacer.classList.add('height-240');
+    // Selected Item
+    const selectedItem = document.createElement('div');
+    selectedItem.classList.add('selected-item');
+    // Append
+    itemContainer.append(topSpacer, selectedItem);
 
-//   // Create Equations, Build Elements in DOM
+    // Create Equations, Build Elements in DOM
+    createEquations();
+    equationsToDOM();
 
-//   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div');
-//   bottomSpacer.classList.add('height-500');
-//   itemContainer.appendChild(bottomSpacer);
-// }
+    // Set Blank Space Below
+    const bottomSpacer = document.createElement('div');
+    bottomSpacer.classList.add('height-500');
+    itemContainer.appendChild(bottomSpacer);
+}
 
 // Display 3 2 1 go
 const countdownStart = () => {
@@ -129,7 +151,8 @@ const selectQuestionAmount = (e) => {
     if (questionAmount) {
         showCountDown();
         countdownStart();
-        createEquations();
+        populateGamePage();
+        setTimeout(showGamePage, 4000);
     }
 };
 
